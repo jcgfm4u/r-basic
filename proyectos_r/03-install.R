4443b631922e7e076480f9858de5b02f13174189
Juan Carlos Guillermo Farías Martínez <jcgfm4u@hotmail.com>
2026-03-18 22:33:13 -0600

    Update 03-install.R

--- proyectos_r/03-install.R
+++ proyectos_r/03-install.R
@@ -1,3 +1,6 @@
+# Fecha: 18/03/2026
+# Autor: Juan Carlos Guillermo Farias Martinez
+
 # Instalación de paquetes en R, con R Studio
 # Para instalar un paquete, se usa install.páckages.
 # Asimismo, se usa el parámetro dep = TRUE para instalar las dependencias necesarias
@@ -11,4 +14,5 @@ install.packages("magic", dep = TRUE)
 
 library(magic)
 
+# Esta, es la ejecución ya del paquete, para crear un cubo mágico que suma la misma cantidad en cualquier dirección
 magic(4)
