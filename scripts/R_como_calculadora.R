12786fa4e0856354d9310cba3b2d5bd52667fcd0
Juan Carlos G. Farias Martinez <jcgfm4u@hotmail.com>
2026-04-05 00:25:01 -0600

    Upd Usando R como calculadora
    Usando R como calculadora.
    
    Usando algunas funciones de redondeo (arriba/abajo), así como funciones de truncamiento.

--- scripts/R_como_calculadora.R
+++ scripts/R_como_calculadora.R
@@ -1,5 +1,39 @@
-# Usando R como calculadora
+R.version.string
 
-2*3+1;
 
-choose(5, 2);
+install.packages("tidyverse", dep = TRUE)
+# Usando R como una calculadora
+1+2
+
+install.packages("magic", dep = TRUE)
+library(magic)
+
+magic(8)
+2+3*5
+sqrt(9)
+
+exp(1)
+
+factorial(8)
+
+choose(10, 2)
+
+factorial(0)
+
+cos(60)
+cos(60*pi/180)
+
+# Redondeo y decimales con R
+# Las principales funcionales para mostrar decimales y/o redondeo
+# Tenemos las siguientes
+# print(x, n) donde x, es el número, fórmula o función, y n es el número de decimales que queremos de ese resultado
+# round(x, n), Redonde n cifras significatvias de un valor, o vector numérico x
+# flookr(x) redondea hacia abajo. Parte entera por defecto de x
+# ceiling(x) Parte entera por exceso de x
+# trunc(x), parte enera de x, eliminando la parte decimal
+
+trunc(sqrt(2))
+print(10, 4)
+
+sqrt(2);
+print(sqrt(2), 10);
